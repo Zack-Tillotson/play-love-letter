7d485395b185639565ec9957fb7ead9e05e4a285
Zack Tillotson <ZackTillotson@gmail.com>
2021-01-25 14:27:46 -0700

    Fleshign out history detail logic

--- src/components/HistoryDetail/HistoryDetail.jsx
+++ src/components/HistoryDetail/HistoryDetail.jsx
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import cn from 'classnames';
 
 import {useSelector, useDispatch} from 'react-redux';
 import selector from '../../state/selector'
@@ -11,7 +12,7 @@ import './historyDetail.scss'
 
 function HistoryDetail() {
   const {cardAction: {historyDetail}, self} = useSelector(selector)
-  const {player, value, statusMessage} = historyDetail;
+  const {player, value, statusMessage, targetPlayer, isCorrectGuess, playerEliminated} = historyDetail;
 
   const dispatch = useDispatch()
 
@@ -19,10 +20,42 @@ function HistoryDetail() {
     dispatch(actions.interactionClick('card-selection-close', true))
   }
 
+  const isTargeted = [1, 2, 3, 5, 6].includes(value);
+  const isPlayerSelf = self.id === player.id
+  const isTargetSelf = self.id === targetPlayer.id
+  const isPlayerEliminated = player.id === playerEliminated?.id
+  const isTargetEliminated = targetPlayer.id === playerEliminated?.id
+
 	return (
-    <div className="history-detail__close" onClick={handleClose}>
-      <Persona {...player} />
-		  <Card value={value} isVisible={true} className="history-detail__card" />
+    <div className={cn('history-detail__close', {['history-detail__close--three-column']: isTargeted})} onClick={handleClose}>
+      <div className="history-detail__player">
+        <Persona {...player}  status={isPlayerEliminated ? 'out' : 'active'} />
+        <Card value={value} isVisible={true} className="history-detail__card" />
+        {[3].includes(value) && (
+          <Card value={player.hand[0] === 3 ? player.hand[1] : player.hand[0]} isVisible={isPlayerSelf || isPlayerEliminated || (!targetPlayer.protected && isTargetSelf)} className="history-detail__card" />
+        )}
+      </div>
+      {isTargeted && ([(
+        <div className="history-detail__targets">
+          @
+        </div>
+        ), (
+        <div className="history-detail__target-player">
+          <Persona {...targetPlayer} status={isTargetEliminated ? 'out' : 'active'} />
+          {value === 1 && (
+            <Card value={targetPlayer.hand[0]} isVisible={!targetPlayer.protected && isTargetSelf || isCorrectGuess} className="history-detail__card" />
+          )}
+          {[2].includes(value) && (
+            <Card value={targetPlayer.hand[0]} isVisible={!targetPlayer.protected && isTargetSelf || isPlayerSelf} className="history-detail__card" />
+          )}
+          {[3].includes(value) && (
+            <Card value={targetPlayer.hand[0]} isVisible={!targetPlayer.protected && isTargetSelf || isTargetEliminated} className="history-detail__card" />
+          )}
+          {[5].includes(value) && (
+            <Card value={targetPlayer.hand[0]} isVisible={!targetPlayer.protected} className="history-detail__card" />
+          )}
+        </div>
+      )])}
       <div className="history-detail__status">{statusMessage}</div>
     </div>
 	)
